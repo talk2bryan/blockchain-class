b7ff5dc106f36866eec0ec70d5ef195b68c3e43d
Bryan Wodi <2839540+talk2bryan@users.noreply.github.com>
2019-09-23 13:44:05 -0500

    removed all multithreading logic

--- a1/WodiA1.py
+++ a1/WodiA1.py
@@ -14,48 +14,54 @@ To RUN:
 
 SAMPLE OUTPUT:
 Started mining block with data: BryanWodi, difficulty: 00
-Mined block hash:  00679e53c1246fd7ae58d0ddf55e7a25b17dde8e17048f8b3784f9b577dfa5a4
+Mined block hash:  00c0567d28b2593c71b7da4a9ba44c4d171a4d32e7176c2d5af921b7b137ab0f
+Mining this block took 0.0008437633514404297 seconds
 Started mining block with data: BryanWodi, difficulty: 000
-Mined block hash:  0002e0088ccf0ae1f5a4a141ad98d53e71e39f67fe85e5bd9649a2db7e815d60
+Mined block hash:  000791019bf32227bfbbb99284f4d61c00102b515787ab69653ec8ce84d42784
+Mining this block took 0.011893749237060547 seconds
 Started mining block with data: BryanWodi, difficulty: 0000
-Mined block hash:  000057657f352462621676f092dc81c7ae7feb9b70c4792ea79e1297b081e9c5
+Mined block hash:  0000daa03fabc1e2376694fe876f17d17c6fbbb3cf0e744be78f6c100777f9cd
+Mining this block took 0.25539684295654297 seconds
 Started mining block with data: BryanWodi, difficulty: 00000
-Mined block hash:  00000329522d0175ba8931c94e2f0824929d3d586bc321252b0cedb4dc973d1b
+Mined block hash:  00000d193e053665029d5ff7c06553e32487a9537ddfa1a629ff258001578138
+Mining this block took 10.553561687469482 seconds
 Started mining block with data: BryanWodi, difficulty: 000000
-Mined block hash:  000000e5c3cf91fc853607592497b7f72523035d346da549ba1d296fc0976028
+Mined block hash:  000000dda38f9c4b45f67d086e98b96513e391348da1edc9734c1441bf120350
+Mining this block took 60.513511657714844 seconds
+Mining has taken longer than 30 seconds, stopping...
 
 
 Printing Blockchain
 ----------------------------------------------------------------------------
-Block Hash: 0c3cfbe5bcafdcd7ae662a58b1686fee1ff5e88986cfee73e41d7aa7e0b5bd4f
+Block Hash: 50263ffd9137935e511273a4c9a1e269f8f3f748515b825c5eaa43f20a3ae8b8
 Block Number: 0
 Block Data: Genesis Block
 Hashes: 0
 ----------------------------------------------------------------------------
-Block Hash: 00679e53c1246fd7ae58d0ddf55e7a25b17dde8e17048f8b3784f9b577dfa5a4
+Block Hash: 00c0567d28b2593c71b7da4a9ba44c4d171a4d32e7176c2d5af921b7b137ab0f
 Block Number: 1
 Block Data: BryanWodi
-Hashes: 58
+Hashes: 115
 ----------------------------------------------------------------------------
-Block Hash: 0002e0088ccf0ae1f5a4a141ad98d53e71e39f67fe85e5bd9649a2db7e815d60
+Block Hash: 000791019bf32227bfbbb99284f4d61c00102b515787ab69653ec8ce84d42784
 Block Number: 2
 Block Data: BryanWodi
-Hashes: 2627
+Hashes: 971
 ----------------------------------------------------------------------------
-Block Hash: 000057657f352462621676f092dc81c7ae7feb9b70c4792ea79e1297b081e9c5
+Block Hash: 0000daa03fabc1e2376694fe876f17d17c6fbbb3cf0e744be78f6c100777f9cd
 Block Number: 3
 Block Data: BryanWodi
-Hashes: 175921
+Hashes: 32415
 ----------------------------------------------------------------------------
-Block Hash: 00000329522d0175ba8931c94e2f0824929d3d586bc321252b0cedb4dc973d1b
+Block Hash: 00000d193e053665029d5ff7c06553e32487a9537ddfa1a629ff258001578138
 Block Number: 4
 Block Data: BryanWodi
-Hashes: 300712
+Hashes: 1634268
 ----------------------------------------------------------------------------
-Block Hash: 000000e5c3cf91fc853607592497b7f72523035d346da549ba1d296fc0976028
+Block Hash: 000000dda38f9c4b45f67d086e98b96513e391348da1edc9734c1441bf120350
 Block Number: 5
 Block Data: BryanWodi
-Hashes: 5436232
+Hashes: 7370657
 ----------------------------------------------------------------------------
 
 End of processing.
@@ -110,9 +116,6 @@ class Blockchain:
 
     dummy = head = block
 
-    def __init__(self):
-        self._lock = threading.Lock()
-
     def add(self, block):
         block.previous_hash = self.block.hash()
         block.block_num = self.block.block_num + 1
@@ -121,39 +124,35 @@ class Blockchain:
         self.block = self.block.next
 
     def mine(self, block, difficulty):
-        with self._lock:
-            print("Started mining block with data: {}, difficulty: {}".format(block.data, difficulty))
-            for n in range(self.max_nonce):
-                if (block.hash()).startswith(difficulty):
-                    block.block_hash = block.hash()
-                    self.add(block)
+        print("Started mining block with data: {}, difficulty: {}".format(block.data, difficulty))
+        for n in range(self.max_nonce):
+            if (block.hash()).startswith(difficulty):
+                block.block_hash = block.hash()
+                self.add(block)
 
-                    # print("Mined block:", block)
-                    print("Mined block hash: ", block.block_hash)
-                    
-                    break
-                else:
-                    block.nonce += 1
+                print("Mined block hash: ", block.block_hash)
+                
+                break
+            else:
+                block.nonce += 1
 
 
 # Main:
 if __name__ == "__main__":
     blockchain = Blockchain()
     num_zeroes = 2
-    max_mining_time_seconds = 3
+    max_mining_time_seconds = 30
     
     start_time = time.time()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    	while True:
-    		executor.submit(blockchain.mine, Block("BryanWodi"), "0"* num_zeroes)
-    		
-    		run_time = time.time() - start_time
-    		print("Mining this block took {} seconds".format(run_time))
-    		if (run_time > max_mining_time_seconds):
-    			print("Mining this block took longer than {} seconds, stopping...".format(max_mining_time_seconds))
-    			break
-    		
-    		num_zeroes += 1
+    while True:
+        blockchain.mine(Block("BryanWodi"), "0"* num_zeroes)
+        run_time = time.time() - start_time
+        print("Mining this block took {} seconds".format(run_time))
+        if (run_time > max_mining_time_seconds):
+            print("Mining has taken longer than {} seconds, stopping...".format(max_mining_time_seconds))
+            break
+        
+        num_zeroes += 1
 
     print("\n\nPrinting Blockchain")
     print("----------------------------------------------------------------------------")
