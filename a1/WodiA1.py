9f3204d6023bc9250106f11aa8a2313ffb95bcdf
Bryan Wodi <2839540+talk2bryan@users.noreply.github.com>
2019-09-23 08:17:05 -0500

    added sample output

--- a1/WodiA1.py
+++ a1/WodiA1.py
@@ -11,6 +11,57 @@ Date: September 22, 2019
 
 To RUN: 
     python3 WodiA1.py
+
+SAMPLE OUTPUT:
+Started mining block with data: BryanWodi, difficulty: 00
+Mined block hash:  00679e53c1246fd7ae58d0ddf55e7a25b17dde8e17048f8b3784f9b577dfa5a4
+Started mining block with data: BryanWodi, difficulty: 000
+Mined block hash:  0002e0088ccf0ae1f5a4a141ad98d53e71e39f67fe85e5bd9649a2db7e815d60
+Started mining block with data: BryanWodi, difficulty: 0000
+Mined block hash:  000057657f352462621676f092dc81c7ae7feb9b70c4792ea79e1297b081e9c5
+Started mining block with data: BryanWodi, difficulty: 00000
+Mined block hash:  00000329522d0175ba8931c94e2f0824929d3d586bc321252b0cedb4dc973d1b
+Started mining block with data: BryanWodi, difficulty: 000000
+Mined block hash:  000000e5c3cf91fc853607592497b7f72523035d346da549ba1d296fc0976028
+
+
+Printing Blockchain
+----------------------------------------------------------------------------
+Block Hash: 0c3cfbe5bcafdcd7ae662a58b1686fee1ff5e88986cfee73e41d7aa7e0b5bd4f
+Block Number: 0
+Block Data: Genesis Block
+Hashes: 0
+----------------------------------------------------------------------------
+Block Hash: 00679e53c1246fd7ae58d0ddf55e7a25b17dde8e17048f8b3784f9b577dfa5a4
+Block Number: 1
+Block Data: BryanWodi
+Hashes: 58
+----------------------------------------------------------------------------
+Block Hash: 0002e0088ccf0ae1f5a4a141ad98d53e71e39f67fe85e5bd9649a2db7e815d60
+Block Number: 2
+Block Data: BryanWodi
+Hashes: 2627
+----------------------------------------------------------------------------
+Block Hash: 000057657f352462621676f092dc81c7ae7feb9b70c4792ea79e1297b081e9c5
+Block Number: 3
+Block Data: BryanWodi
+Hashes: 175921
+----------------------------------------------------------------------------
+Block Hash: 00000329522d0175ba8931c94e2f0824929d3d586bc321252b0cedb4dc973d1b
+Block Number: 4
+Block Data: BryanWodi
+Hashes: 300712
+----------------------------------------------------------------------------
+Block Hash: 000000e5c3cf91fc853607592497b7f72523035d346da549ba1d296fc0976028
+Block Number: 5
+Block Data: BryanWodi
+Hashes: 5436232
+----------------------------------------------------------------------------
+
+End of processing.
+Author: Bryan Wodi
+
+
 '''
 
 import concurrent.futures
@@ -118,6 +169,7 @@ if __name__ == "__main__":
         print(blockchain.head)
         blockchain.head = blockchain.head.next
 
+    print("\nEnd of processing.\nAuthor: Bryan Wodi")
 
     
 
